6281d6d9a5b051cb5ce693090016122b365bb93d
Antonin-Caussin <antonin.caussin@student.uliege.be>
2025-04-15 12:50:51 +0200

    pour le check, je dois supprimer ca

--- R/equations.R
+++
@@ -1,5 +0,0 @@
-equations_allometriques <- read.csv2("C:/Users/anton/Desktop/TFE/Ecriture/Equations.csv",
-                                     sep = "\t",
-                                     stringsAsFactors = FALSE,
-                                     na.strings = c("NA", ""))
-usethis::use_data(equations_allometriques, internal = TRUE, overwrite = TRUE)
