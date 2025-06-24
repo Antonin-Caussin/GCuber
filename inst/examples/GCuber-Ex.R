fe48822cf0d73298704eba6de5c2474bfaffea73
Antonin-Caussin <antonin.caussin@student.uliege.be>
2025-06-24 14:40:11 +0200

    DHB -> D130

--- inst/examples/GCuber-Ex.R
+++ inst/examples/GCuber-Ex.R
@@ -1,8 +1,1 @@
 
-# Exemple d'utilisation de la fonction Biomasse
-data <- data.frame(essence = 'Bouleau', volume = 100)
-resultat <- Biomasse(data, type = 'VC22')
-print(resultat)
-# Exemple d'utilisation de la fonction liste_eq
-liste_eq(essence = "Bouleau", type_volume = "VC22")
-
